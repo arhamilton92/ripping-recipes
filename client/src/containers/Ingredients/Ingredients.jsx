b5cd8fe0a13e8145fb6f02c7f81ccf443db999c4
Andrea RH <andrea.r.hamilton92@gmail.com>
2020-08-13 10:57:41 -0400

    created ingredients model and added route in app

---
+++ client/src/containers/Ingredients/Ingredients.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Ingredients = () => {
+    return (
+        <div>
+            My ingredients will go here.
+        </div>
+    );
+};
+
+export default Ingredients;
