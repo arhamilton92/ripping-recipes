fec8b945db7171b31df8083430945b1feb506089
Andrea RH <andrea.r.hamilton92@gmail.com>
2020-08-14 08:35:31 -0400

    can create and display ingredients via web page

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./containers/Home/Home";
 import NoMatch from "./containers/NoMatch/NoMatch";
 import Ingredients from './containers/Ingredients/Ingredients';
+import NewIngredient from './containers/NewIngredient/NewIngredient';
 
 function App() {
   useEffect(() => {
@@ -21,6 +22,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/ingredients" component={Ingredients} />
+        <Route exact path="/ingredients/new" component={NewIngredient} />
         <Route component={NoMatch} />
       </Switch>
     </Router>
--- client/src/containers/Ingredients/Ingredients.jsx
+++ client/src/containers/Ingredients/Ingredients.jsx
@@ -1,11 +1,57 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
+import {Link} from "react-router-dom";
 
-const Ingredients = () => {
-    return (
-        <div>
-            My ingredients will go here.
-        </div>
-    );
+class Ingredients extends Component {
+
+    state = {
+        ingredients: []
+    }
+
+    componentDidMount() {
+        axios.get("/api/ingredients")
+        .then((response) => {
+            this.setState({
+                ingredients: response.data.data
+            })
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col s9">
+                        <h1>Ingredients</h1>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col s3">
+                        <h5>
+                            <Link to="/ingredients/new">Create a new ingredient</Link>
+                        </h5>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col s12">
+                        {this.state.ingredients.map((ingredient)=> (
+                            <div key={ingredient._id}>
+                                <h3>{ingredient.name}</h3>
+
+                            </div>
+
+                        ))}
+                    </div>
+                    <div className="col s12">
+                        {this.state.ingredients.length === 0 && <p>No ingredients returned.</p>}
+                    </div>
+                </div>
+            </div>
+        );
+    }
 };
 
 export default Ingredients;
